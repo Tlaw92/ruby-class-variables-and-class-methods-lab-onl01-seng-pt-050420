688b41c7c15a7075499e5061263b5f9dc1cb4ecd
travis Lawson <travislawclimb@gmail.com>
2020-06-26 23:36:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,49 @@
 class Song 
   
-  
-  @@song_list = []
-  @@count = @@song_list.length
-  @@artists = []
-  @@genres = []
-  @@artist_count = {}
-  @@genre_count = {}
-  
-  attr_accessor :name, :artist, :genre
-  
-  
   def initialize(name, artist, genre)
-    @@song_list.push self
-    @name = name
+    @name = name 
     @artist = artist 
     @genre = genre 
-    
-    @@song_list(self)
   end   
   
-  
 end   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song 
   
-Song.new  
+  
+#   @@song_list = []
+#   @@count = @@song_list.length
+#   @@artists = []
+#   @@genres = []
+#   @@artist_count = {}
+#   @@genre_count = {}
+  
+#   attr_accessor :name, :artist, :genre
+  
+  
+#   def initialize(name, artist, genre)
+#     @@song_list.push self
+#     @name = name
+#     @artist = artist 
+#     @genre = genre 
+    
+#     @@song_list(self)
+#   end   
+  
+  
+# end   
+  
+# Song.new  
